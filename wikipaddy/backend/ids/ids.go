9ea3a53745061ec3fe3ece6ac28d805e003fcebd
sibobbbbbb <114354358+sibobbbbbb@users.noreply.github.com>
2024-04-23 10:31:04 +0700

    Update ids.go

--- wikipaddy/backend/ids/ids.go
+++ wikipaddy/backend/ids/ids.go
@@ -1,6 +1,7 @@
 package ids
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -9,21 +10,21 @@ import (
 )
 
 type WikiRacerIDS struct {
-	startURL   string
-	endURL     string
-	visited    map[string]int
-	pageLinks  map[string][]string
-	maxDepth   int
+	startURL      string
+	endURL        string
+	visited       map[string]int
+	pageLinks     map[string][]string
+	maxDepth      int
 	linksExamined int // Number of links examined
 }
 
 func NewWikiRacerIDS(startURL, endURL string) *WikiRacerIDS {
 	return &WikiRacerIDS{
-		startURL:  startURL,
-		endURL:    endURL,
-		visited:   make(map[string]int),
-		pageLinks: make(map[string][]string),
-		maxDepth:  0,
+		startURL:      startURL,
+		endURL:        endURL,
+		visited:       make(map[string]int),
+		pageLinks:     make(map[string][]string),
+		maxDepth:      0,
 		linksExamined: 1,
 	}
 }
@@ -51,18 +52,26 @@ func (wr *WikiRacerIDS) fetchLinks(pageURL string) ([]string, error) {
 	return links, nil
 }
 
-func (wr *WikiRacerIDS) depthLimitedSearch(currentURL string, depth int) (bool, []string) {
+func (wr *WikiRacerIDS) depthLimitedSearch(currentURL string, depth int, path []string) (bool, []string) {
 	if depth > wr.maxDepth {
 		return false, nil
 	}
 
 	if currentURL == wr.endURL {
-		return true, []string{wr.endURL}
+		// Append endURL to the path
+		path = append(path, currentURL)
+		return true, path
 	}
 
 	// Mark the current page as visited at the current depth.
 	wr.visited[currentURL] = depth
 	wr.linksExamined++
+
+	// Append currentURL to the path
+	path = append(path, currentURL)
+
+	// Print the path from startURL to currentURL
+	printPath(path)
 
 	links, err := wr.fetchLinks(currentURL)
 	if err != nil {
@@ -72,10 +81,10 @@ func (wr *WikiRacerIDS) depthLimitedSearch(currentURL string, depth int) (bool, 
 
 	for _, link := range links {
 		if prevDepth, visited := wr.visited[link]; !visited || depth+1 < prevDepth {
-			found, path := wr.depthLimitedSearch(link, depth+1)
+			found, path := wr.depthLimitedSearch(link, depth+1, path)
 			if found {
-				// Prepend currentURL to the path and return.
-				return true, append([]string{currentURL}, path...)
+				// Return the complete path
+				return true, path
 			}
 		}
 	}
@@ -85,7 +94,7 @@ func (wr *WikiRacerIDS) depthLimitedSearch(currentURL string, depth int) (bool, 
 
 func (wr *WikiRacerIDS) FindShortestPathUsingIDS() ([]string, error) {
 	for {
-		found, path := wr.depthLimitedSearch(wr.startURL, 0)
+		found, path := wr.depthLimitedSearch(wr.startURL, 0, []string{})
 		if found {
 			return path, nil
 		}
@@ -95,5 +104,33 @@ func (wr *WikiRacerIDS) FindShortestPathUsingIDS() ([]string, error) {
 }
 
 func (wr *WikiRacerIDS) LinksExamined() int {
-    return wr.linksExamined
+	return wr.linksExamined
+}
+
+// extractArticleTitle extracts the title of the Wikipedia article from the URL
+func extractArticleTitle(url string) string {
+	// Remove "https://en.wikipedia.org/wiki/" from the beginning of the URL
+	title := strings.TrimPrefix(url, "https://en.wikipedia.org/wiki/")
+
+	// Find the index of the first "/" after "/wiki/"
+	index := strings.Index(title, "/")
+	if index != -1 {
+		// If found "/", take the part before it as the article title
+		title = title[:index]
+	}
+
+	return title
+}
+
+// printPath prints the path from startURL to currentURL
+func printPath(path []string){
+	if len(path) == 0 {
+		return
+	}
+
+	fmt.Printf("%s", extractArticleTitle(path[0]))
+	for i := 1; i < len(path); i++ {
+		fmt.Printf(" -> %s", extractArticleTitle(path[i]))
+	}
+	fmt.Println()
 }
